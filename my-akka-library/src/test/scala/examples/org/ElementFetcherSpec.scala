ac1b0da55556db6522d58c6727063e0f292fec3e
Alvaro Viliplana Garcia <alvaro.viliplanagarcia@sky.uk>
2023-02-01 20:42:18 +0000

    Terminate the actor system into the test

--- my-akka-library/src/test/scala/examples/org/ElementFetcherSpec.scala
+++ my-akka-library/src/test/scala/examples/org/ElementFetcherSpec.scala
@@ -5,9 +5,18 @@ import flatspec._
 import matchers._
 import akka.actor.ActorSystem
 import org.scalatest.concurrent.ScalaFutures
-class ElementFetcherSpec extends AnyFlatSpec with should.Matchers with ScalaFutures {
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+class ElementFetcherSpec extends AnyFlatSpec with should.Matchers with ScalaFutures with BeforeAndAfterAll {
 
   implicit val system: ActorSystem = ActorSystem("reactive-tweets")
+
+  override def afterAll() = {
+    system.terminate
+    Await.ready(system.whenTerminated, Duration(1, TimeUnit.MINUTES))
+  }
 
   "An ElementFetcher" should "fetch the elements" in {
     val elements = ElementFetcher.fetchList(system).futureValue
