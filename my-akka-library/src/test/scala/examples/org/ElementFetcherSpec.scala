17d538db6778416571f2a18967b66ac244b577c0
Author Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2023-02-01 16:26:54 +0000

    Fix tests

--- my-akka-library/src/test/scala/examples/org/ElementFetcherSpec.scala
+++ my-akka-library/src/test/scala/examples/org/ElementFetcherSpec.scala
@@ -5,17 +5,13 @@ import flatspec._
 import matchers._
 import akka.actor.ActorSystem
 import org.scalatest.concurrent.ScalaFutures
-
 class ElementFetcherSpec extends AnyFlatSpec with should.Matchers with ScalaFutures {
 
   implicit val system: ActorSystem = ActorSystem("reactive-tweets")
 
   "An ElementFetcher" should "fetch the elements" in {
     val elements = ElementFetcher.fetchList(system).futureValue
-    elements should contain theSameElementsInOrderAs(
-      "this is the element 1",
-      "this is the element 2",
-      "this is the element 3"
-    )
+    elements should contain theSameElementsAs
+      Iterable("this is the element 1", "this is the element 2", "this is the element 3")
   }
 }
